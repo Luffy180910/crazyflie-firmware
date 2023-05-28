d40bfaf2be5c157c38269fc3362fc0d601929412
hyx <hyx18234172491@163.com>
2023-05-28 16:09:55 +0800

    to:周期改为60ms

--- src/deck/drivers/src/swarm_ranging.c
+++ src/deck/drivers/src/swarm_ranging.c
@@ -298,7 +298,7 @@ static void uwbRangingTxTask(void *parameters)
       notget_packet_interval = xTaskGetTickCount() - neighbor_latest_rvTime[address];
       if (notget_packet_interval > 2 * TX_PERIOD_IN_MS + 5)
       {
-        if (get_tx_rx_min_interval(address) <= 2 || notget_packet_interval >= 180|| notget_packet_interval > 4 * TX_PERIOD_IN_MS)
+        if (get_tx_rx_min_interval(address) <= 2 || notget_packet_interval >= 190|| notget_packet_interval > 4 * TX_PERIOD_IN_MS)
         {
           nextTransportPeriod = 5 + rand() % (TX_PERIOD_IN_MS - 5);
           break;
