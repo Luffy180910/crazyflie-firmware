3cdf43b62b76b5ed7b8cf2b5f1853d589a7c7ad6
Huo Haodong <huohaodong.io@gmail.com>
2023-06-17 17:21:24 +0800

    update sniffer script

--- sniffer.py
+++ sniffer.py
@@ -1,4 +1,8 @@
+import datetime
+
 import usb.util
+import struct
+import pickle
 
 if __name__ == '__main__':
     vendor_id = 0x0483
@@ -13,16 +17,31 @@ if __name__ == '__main__':
 
     endpoint = dev[0][(0, 0)][0]
 
+    log_data = []
+
     try:
         while True:
+            meta = dev.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
             try:
-                data = dev.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
-                if len(data) > 0:
-                    print(data)
-            except:
+                magic, sender_addr, seq_num, msg_len, sniffer_rx_time = struct.unpack("<IHHHQ", meta)
+                meta_dict = {'magic': magic, 'sender_addr': sender_addr, 'seq_num': seq_num, 'msg_len': msg_len,
+                             'sniffer_rx_time': sniffer_rx_time}
+                print(meta_dict)
+            except struct.error:
                 pass
+            else:
+                if magic == 0xBB88:
+                    meta_dict['bin_data'] = dev.read(endpoint.bEndpointAddress, msg_len)
+                    log_data.append(meta_dict)
     except KeyboardInterrupt:
         pass
+    finally:
+        if len(log_data) > 0:
+            with open('./data/' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.pkl', 'wb') as file:
+                pickle.dump(log_data, file)
+        usb.util.release_interface(dev, 0)
+        usb.util.dispose_resources(dev)
 
-    usb.util.release_interface(dev, 0)
-    usb.util.dispose_resources(dev)
+    # with open('filename', 'rb') as file:
+    #     data = pickle.load(file)
+    # print(data)
