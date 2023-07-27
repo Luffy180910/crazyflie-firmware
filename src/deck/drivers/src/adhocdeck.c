25518f70d5d7165558bdb1fc49f72e6db3ba757b
hyx <18234172491@163.com>
2023-07-27 21:10:53 +0800

    测量接收中断时间

--- src/deck/drivers/src/adhocdeck.c
+++ src/deck/drivers/src/adhocdeck.c
@@ -85,6 +85,7 @@ static void txCallback()
 
 static void rxCallback(dwt_cb_data_t *cbData)
 {
+  dwt_readsystime((uint8_t *)&rxTime2.raw);
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
   uint32_t dataLength = cbData->datalength;
@@ -94,7 +95,7 @@ static void rxCallback(dwt_cb_data_t *cbData)
   dwt_readrxdata(rxBuffer, dataLength - FCS_LEN, 0); /* No need to read the FCS/CRC. */
 
   dwt_readrxtimestamp((uint8_t *)&rxTime1.raw);
-  dwt_readsystime((uint8_t *)&rxTime2.raw);
+  
   // DEBUG_PRINT("%llu,%llu\n", rxTime1.full, rxTime2.full);
 
   UWB_Packet_t *packet = (UWB_Packet_t *)&rxBuffer;
