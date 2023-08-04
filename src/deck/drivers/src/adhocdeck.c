261a8dce76d93194b0f93e04df16dbb772487480
Li Chunyi <1403198796@qq.com>
2023-08-04 01:48:06 -0700

    增加了双缓冲的开关

--- src/deck/drivers/src/adhocdeck.c
+++ src/deck/drivers/src/adhocdeck.c
@@ -82,17 +82,22 @@ static void txCallback()
 
 static void rxCallback(dwt_cb_data_t *cbData)
 {
-  dwt_rxenable(DWT_START_RX_IMMEDIATE);
+  #ifdef ENABLE_DBL
+    dwt_rxenable(DWT_START_RX_IMMEDIATE);
+  #endif
+
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
   uint32_t dataLength = cbData->datalength;
 
-  uint8_t statusDB = dwt_read8bitoffsetreg(RDB_STATUS_ID, 0);
-  if ((statusDB == 0x70 || statusDB == 0x07 || statusDB == 0x77))
-  {
-    dwt_signal_rx_buff_free();
-    dataLength = dwt_read32bitreg(RX_FINFO_ID) & RX_FINFO_RXFLEN_BIT_MASK;
-  }
+  #ifdef ENABLE_DBL
+    uint8_t statusDB = dwt_read8bitoffsetreg(RDB_STATUS_ID, 0);
+    if ((statusDB == 0x70 || statusDB == 0x07 || statusDB == 0x77))
+    {
+      dwt_signal_rx_buff_free();
+      dataLength = dwt_read32bitreg(RX_FINFO_ID) & RX_FINFO_RXFLEN_BIT_MASK;
+    }
+  #endif
 
   ASSERT(dataLength != 0 && dataLength <= FRAME_LEN_MAX);
 
@@ -212,8 +217,11 @@ static int uwbInit()
    * wait timeout), the receiver will re-enable to re-attempt reception. */
   dwt_or32bitoffsetreg(SYS_CFG_ID, 0, SYS_CFG_RXAUTR_BIT_MASK);
   dwt_setrxtimeout(DEFAULT_RX_TIMEOUT);                     // in microseconds (1.0256 us).
-  dwt_setdblrxbuffmode(DBL_BUF_STATE_EN, DBL_BUF_MODE_MAN); // Enable double buff - Manual mode
-  dwt_configciadiag(DW_CIA_DIAG_LOG_MIN);                   // Enable diagnostic mode - minimal
+
+  #ifdef ENABLE_DBL
+    dwt_setdblrxbuffmode(DBL_BUF_STATE_EN, DBL_BUF_MODE_MAN); // Enable double buff - Manual mode
+    dwt_configciadiag(DW_CIA_DIAG_LOG_MIN);                   // Enable diagnostic mode - minimal
+  #endif
 
   dwt_setcallbacks(&txCallback, &rxCallback, &rxTimeoutCallback, &rxErrorCallback, NULL, NULL);
   /* Enable wanted interrupts (TX confirmation, RX good frames, RX timeouts and
@@ -250,25 +258,29 @@ static void uwbTxTask(void *parameters)
       packetCache.header.srcAddress = MY_UWB_ADDRESS;
       packetCache.header.seqNumber = packetSeqNumber++;
       ASSERT(packetCache.header.length <= FRAME_LEN_MAX);
-      uint8_t fstat = dwt_read8bitoffsetreg(FINT_STAT_ID, 0);
-      uint32_t status = dwt_read32bitreg(SYS_STATUS_ID);
-      uint8_t statusDB = dwt_read8bitoffsetreg(RDB_STATUS_ID, 0);
-      // DEBUG_PRINT("tx-DB: %02X\n", statusDB);
-      if (status)
-      {
-        // DEBUG_PRINT("=====================\n");
-        // DEBUG_PRINT("FINT: 0x%lx\tSYS_S: 0x%lx\n", fstat, status);
-        if (status & SYS_STATUS_RXOVRR_BIT_MASK)
+
+      #ifdef ENABLE_DBL
+        uint8_t fstat = dwt_read8bitoffsetreg(FINT_STAT_ID, 0);
+        uint32_t status = dwt_read32bitreg(SYS_STATUS_ID);
+        uint8_t statusDB = dwt_read8bitoffsetreg(RDB_STATUS_ID, 0);
+        // DEBUG_PRINT("tx-DB: %02X\n", statusDB);
+        if (status)
         {
-          DEBUG_PRINT("SYS_STATUS_RXOVRR_BIT_MASK\n");
-          dwt_signal_rx_buff_free();
-          dwt_signal_rx_buff_free();
-          dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXOVRR_BIT_MASK);
+          // DEBUG_PRINT("=====================\n");
+          // DEBUG_PRINT("FINT: 0x%lx\tSYS_S: 0x%lx\n", fstat, status);
+          if (status & SYS_STATUS_RXOVRR_BIT_MASK)
+          {
+            DEBUG_PRINT("SYS_STATUS_RXOVRR_BIT_MASK\n");
+            dwt_signal_rx_buff_free();
+            dwt_signal_rx_buff_free();
+            dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXOVRR_BIT_MASK);
+          }
+          vTaskNotifyGiveFromISR(uwbTaskHandle, pdTRUE);
+          // DEBUG_PRINT("NotifyGive\n");
+          taskYIELD();
         }
-        vTaskNotifyGiveFromISR(uwbTaskHandle, pdTRUE);
-        // DEBUG_PRINT("NotifyGive\n");
-        taskYIELD();
-      }
+      #endif
+
       dwt_forcetrxoff();
       dwt_writetxdata(packetCache.header.length, (uint8_t *)&packetCache, 0);
       dwt_writetxfctrl(packetCache.header.length + FCS_LEN, 0, 1);
