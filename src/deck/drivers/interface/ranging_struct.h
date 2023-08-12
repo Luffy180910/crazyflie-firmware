b4f9013f84419bc275c8eac53f159d4363bdb92b
hyx <18234172491@163.com>
2023-08-12 15:20:09 +0800

    将MAX_BODY_UNIT_NUMBER设置为29

--- src/deck/drivers/interface/ranging_struct.h
+++ src/deck/drivers/interface/ranging_struct.h
@@ -7,7 +7,7 @@
 #include "dwTypes.h"
 #include "adhocdeck.h"
 
-#define MAX_BODY_UNIT_NUMBER 15
+#define MAX_BODY_UNIT_NUMBER 29
 //#define MAX_BODY_UNIT_NUMBER (FRAME_LEN_MAX - sizeof(Ranging_Message_Header_t)) / sizeof(Body_Unit_t) // 1 ~ 83
 #define RANGING_TABLE_SIZE 25
 #define RANGING_TABLE_HOLD_TIME 10000
