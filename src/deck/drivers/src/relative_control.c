7651c92cc3b5ee86b9cf85c664086ce4240d04c8
Liudamie <jiangpengliu0712@163.com>
2023-08-19 19:05:15 +0800

    重新更改了跟随阶段targetlist的索引

--- src/deck/drivers/src/relative_control.c
+++ src/deck/drivers/src/relative_control.c
@@ -441,8 +441,8 @@ void relativeControlTask(void *arg)
           { int8_t index = MY_UWB_ADDRESS;
             if( MY_UWB_ADDRESS > 8 )
             int8_t index = MY_UWB_ADDRESS + (MY_UWB_ADDRESS - 9)/3;
-            targetX = -cosf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[MY_UWB_ADDRESS][STATE_rlX] + sinf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[MY_UWB_ADDRESS][STATE_rlY];
-            targetY = -sinf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[MY_UWB_ADDRESS][STATE_rlX] - cosf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[MY_UWB_ADDRESS][STATE_rlY];
+            targetX = -cosf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[index][STATE_rlX] + sinf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[index][STATE_rlY];
+            targetY = -sinf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[index][STATE_rlX] - cosf(relaVarInCtrl[0][STATE_rlYaw]) * targetList[index][STATE_rlY];
             formation0asCenter(targetX, targetY, set_height);
           }
         }
